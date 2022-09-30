7a38ebf9dfc1c13e57ee412ad75892820e2d56d7
Talha Ali <alitalha113@gmail.com>
2022-09-30 13:14:22 -0800

    idk

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/create_page", views.create_page, name="create_page"),
+    path("wiki/<str:title>/", views.entry_page, name="title"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import markdown2
+markdowner = markdown2.Markdown()
 
 from . import util
 
@@ -8,3 +10,16 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def create_page(request):
+    pass
+
+def entry_page(request, title):
+    entry = title
+    my_entry = util.get_entry(entry)
+    page_converted = markdowner.convert(my_entry)
+    return render(request, "encyclopedia/show_links.html", {"page_converted":page_converted, "entry": entry})
+    # if request.method == 'POST':
+    #     current_entries = request.POST.get('TITLE')
+    #     my_entry = util.get_entry(current_entries)
+    #     return render(request, "encyclopedia/links.html", {"my_entry":my_entry})
+    # #return render(request, "encyclopedia/links.html")
