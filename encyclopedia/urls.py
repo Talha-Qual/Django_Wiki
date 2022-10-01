a92ffad4708931cec137263176fc4faef8bc7eb7
Talha Ali <alitalha113@gmail.com>
2022-10-01 14:48:37 -0800

    x

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,7 @@ app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/create_page", views.create_page, name="create_page"),
+    path("wiki/random_page", views.random_page, name="random_page"),
     path("wiki/<str:title>/", views.entry_page, name="title"),
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+import re
+from distutils.log import error
+from django.shortcuts import render, redirect
+import random
+from django.urls import reverse
+from django.contrib import messages
 import markdown2
 markdowner = markdown2.Markdown()
 
@@ -10,16 +15,52 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def create_page(request):
-    pass
+def save(request, **kwargs):
+    title = kwargs.get("title", "")
+    content = kwargs.get("content", "")
+    if content and title:
+        title = title.strip()
+        entry = util.save_entry(title.strip(), str(content).strip())
+        return redirect(reverse(index), title=title)
+
+def create_page(request, title = ""):
+    previous_title = title
+    if request.method == "POST":
+        title = request.POST.get("title", "").strip()
+        content = request.POST.get("content", "").strip()
+        submit = request.POST.get("submit")
+        hidden = request.POST.get("config")
+
+        action = "updated" if "edit" in hidden else "created"
+        messages.success(request, f" Your entry was {action} succesfully!")
+        return save(request, title=title, content =content)
+        # util.save_entry(title=title, content=content)
+        # return redirect(reverse(index), title=title)
+        # return render(request, "encyclopedia/index.html", content)
+    else:
+        context = {"config": "create"}
+        if title:
+            entry = util.get_entry(title)
+            if not entry or entry is None:
+                return error(msg="Page Not Found")
+            context["entry"] = entry
+            context["config"] = "edit"
+
+        context.update(
+            {
+                "title": title, "unavailable_entry": util.list_entries()
+            }
+        )
+    return render(request, "encyclopedia/create_edit.html", context)
 
 def entry_page(request, title):
     entry = title
     my_entry = util.get_entry(entry)
     page_converted = markdowner.convert(my_entry)
     return render(request, "encyclopedia/show_links.html", {"page_converted":page_converted, "entry": entry})
-    # if request.method == 'POST':
-    #     current_entries = request.POST.get('TITLE')
-    #     my_entry = util.get_entry(current_entries)
-    #     return render(request, "encyclopedia/links.html", {"my_entry":my_entry})
-    # #return render(request, "encyclopedia/links.html")
+
+def random_page(request):
+    entries = random.choice(util.list_entries())
+    my_entry = util.get_entry(entries)
+    random_page_converted = markdowner.convert(my_entry)
+    return render(request, "encyclopedia/show_links.html", {"random_page_converted":random_page_converted, "entries": entries})
