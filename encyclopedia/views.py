fe559e67cebf95b817ebc8d6b2f43de300044eb4
Talha Ali <alitalha113@gmail.com>
2022-10-02 15:04:20 -0800

    create_page not working with redirect

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,5 @@
+from http.client import HTTPResponse
+from django.http import HttpResponseRedirect
 import re
 from distutils.log import error
 from django.shortcuts import render, redirect
@@ -24,7 +26,18 @@ def index(request):
 #         entry = util.save_entry(title.strip(), str(content).strip())
 #         return redirect(reverse(index), title=title)
 
+def update_page(request, title):
+    curr_title = title # current title of the page is passed in
+    curr_content = util.get_entry(curr_title) # current contents of the page is passed in
+
+    new_title = curr_title # Enter new title in the form in the html
+    new_content = curr_content # Enter new content in the form in the html
+
+    context = {"new_title":new_title, "new_content": new_content}
+
+
 def create_page(request, title = ""):
+    context = {"config": "create"}
     previous_title = title
     if request.method == "POST":
         title = request.POST.get("title", "").strip()
@@ -32,10 +45,20 @@ def create_page(request, title = ""):
         submit = request.POST.get("submit")
         hidden = request.POST.get("config")
 
+        if submit is None:
+            if "create" in hidden:
+                return render(request, "encyclopedia/create.html", context)
+            else:
+                return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
+        elif not title or not content:
+            error(msg= "Must add title and content to create entry")
+            return render(request, "encyclopedia/create.html", context)
+
         action = "updated" if "edit" in hidden else "created"
         messages.success(request, f" Your entry was {action} succesfully!")
         util.save_entry(title=title, content=content)
-        return render(request, "encyclopedia/index.html", {"entries": util.list_entries()} )
+        return HttpResponseRedirect(reverse(index))
+        #return render(request, "encyclopedia/index.html", {"entries": util.list_entries()} )
     else:
         context = {"config": "create"}
         if title:
@@ -50,11 +73,12 @@ def create_page(request, title = ""):
                 "title": title, "unavailable_entry": util.list_entries()
             }
         )
-    return render(request, "encyclopedia/create_edit.html", context)
+    return render(request, "encyclopedia/create.html", context)
 
 def entry_page(request, title):
     entry = title
     my_entry = util.get_entry(entry)
+    # will probably use the bottom two lines when updating the page
     page_converted = markdowner.convert(my_entry)
     return render(request, "encyclopedia/show_links.html", {"page_converted":page_converted, "entry": entry})
 
